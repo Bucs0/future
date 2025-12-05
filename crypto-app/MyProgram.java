f5c33da5cedf00af47e8aab5356f9f10016ea7f0
Ryuujiflare <ryuujiflare@gmail.com>
2025-12-06 03:40:47 +0800

    2ND Commit

--- crypto-app/MyProgram.java
+++ crypto-app/MyProgram.java
@@ -1,6 +1,17 @@
-public class MyProgram {
-    public static void main(String[] args) {
-        // Your code goes here
-        System.out.println("Hello, Java!");
+
+    import javax.swing.JFrame;
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+    public class MyProgram{
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("My First Swing GUI");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 300); 
+            frame.setVisible(true); 
+
+            JPanel panel = new JPanel();
+            frame.add(panel);
+        }
     }
-}
